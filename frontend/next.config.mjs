ff998f1435c360b849fd8f11e6a1dcbb26dbb34b
Your Name <you@example.com>
2026-09-22 00:42:39 +0800

    fix: change build command to next-on-pages for cloudflare deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NODE_ENV === 'development' 
+          ? 'http://localhost:8787/api/:path*' 
+          : 'https://tugasmu-api.johananggo.workers.dev/api/:path*',
+      },
+    ]
+  },
+};
 
 export default nextConfig;
