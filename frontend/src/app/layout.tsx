91a9080037d81c86e43fd4331a0dfdaea079438e
Your Name <you@example.com>
2026-09-22 00:22:22 +0800

    chore: add google analytics ID

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <head>
         {/* Google Analytics 4 */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          src="https://www.googletagmanager.com/gtag/js?id=G-8K10RNGN76"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -45,7 +45,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){window.dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
+            gtag('config', 'G-8K10RNGN76');
           `}
         </Script>
         
